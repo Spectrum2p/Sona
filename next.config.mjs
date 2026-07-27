87c4712f4d844e6f4a2cbb0c062f0dbae4f1290d
Spectrum2p <muatnu@gmail.com>
2026-07-27 12:47:37 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import withPWAInit from 'next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  turbopack: {}, // 👈 Tambahkan baris ini untuk membungkam error Turbopack Next.js 16
+};
+
+export default withPWA(nextConfig);
